591c71901a633392261d3340009f24b35d6b46eb
stevedeey <stvolutosin69@gmail.com>
2021-09-21 13:34:48 +0100

    Initial Commit

---
+++ src/main/java/com/digicore/digicore_banking_web_application/DigicoreBankingWebApplication.java
@@ -0,0 +1,13 @@
+package com.digicore.digicore_banking_web_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicoreBankingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigicoreBankingWebApplication.class, args);
+    }
+
+}
