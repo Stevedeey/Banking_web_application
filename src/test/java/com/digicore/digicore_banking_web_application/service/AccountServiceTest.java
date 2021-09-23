92b09854b0bd3ca77f479dc3f9e87d02eb6d09dc
stevedeey <stvolutosin69@gmail.com>
2021-09-23 10:55:55 +0100

    create unit test for createAccount Implementation

--- src/test/java/com/digicore/digicore_banking_web_application/service/AccountServiceTest.java
+++ src/test/java/com/digicore/digicore_banking_web_application/service/AccountServiceTest.java
@@ -1,6 +1,8 @@
 package com.digicore.digicore_banking_web_application.service;
 
 import com.digicore.digicore_banking_web_application.model.AccountEntity;
+import com.digicore.digicore_banking_web_application.payload.requests.CreateAccountRequest;
+import com.digicore.digicore_banking_web_application.utility.Util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,28 +39,51 @@ class AccountServiceTest {
                 .accountName("Olaleye Oluwatosin")
                 .balance(900.00)
                         .build();
-
         accountMap.put(accountEntity.getAccountNumber(), accountEntity);
 
 
+
+
+
+
     }
 
 
 
     @Test
-    public void testCaseForCreateAccount(){
+    public void testCaseForWhenAccountCreationWasSuccessfull(){
+
+        var accountReq = new CreateAccountRequest();
+        accountReq.setAccountName("Daro Joseph");
+        accountReq.setInitialDeposit(500.0);
+        accountReq.setAccountPassword("1234567890");
+
+        var response = accountService.createAccount(accountReq);
+
+        assertEquals(200, response.getResponseCode());
+        assertTrue(response.isSuccess());
+        assertNotNull((Object)"Account successfully created, Your new account number is 9941837309", response.getMessage() );
+
+        // Testing for account creation with same account name
+
+        accountReq.setAccountName("Daro Joseph");
+
+        var response2 = accountService.createAccount(accountReq);
+        assertEquals(400, response2.getResponseCode());
+        assertFalse(response2.isSuccess());
+        assertEquals("User with this name already exist", response2.getMessage());
+
+        // Testing for an initial deposit that is less than 500
+
+        accountReq.setInitialDeposit(1.0);
+        accountReq.setAccountName("Joseph Daro");
+
+        var response3 = accountService.createAccount(accountReq);
+        assertEquals(400, response3.getResponseCode());
+        assertFalse(response3.isSuccess());
+        assertEquals("Your initial deposit cannot be less than 500", response3.getMessage());
 
 
     }
 
-    @Test
-    public void testCaseForWhenAccountNumberIsSuppliedToFetchAccountInfo(){
-
-        String accountNumber = "1234567890";
-
-        AccountEntity accountEntity = accountMap.get(accountNumber);
-
-        assertEquals(accountNumber, accountEntity.getAccountNumber());
-
-    }
 }
